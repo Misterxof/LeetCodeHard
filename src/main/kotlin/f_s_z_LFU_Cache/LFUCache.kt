2e2f53ce64eb8315ca8abbe40fd6bf183ef1074d
Misterxof <mishail.lukushevich@gmail.com>
2023-01-29 18:15:43 +0300

    Failed test 18 of 460 LFU Cache
    Failed test 18 of 460 LFU Cache, and the solution is bad

--- src/main/kotlin/f_s_z_LFU_Cache/LFUCache.kt
+++ src/main/kotlin/f_s_z_LFU_Cache/LFUCache.kt
@@ -1,7 +1,7 @@
 package f_s_z_LFU_Cache
 
 class LFUCache(capacity: Int) {
-    private val cacheHashMap: HashMap<Int, Pair<Int, Int>>
+    private val cacheHashMap: MutableList<Triple<Int, Int, Int>>
     private var capacity = 0
     private var currentCapacity = 0
     private var lastUsedKey: MutableList<Int>
@@ -9,70 +9,137 @@ class LFUCache(capacity: Int) {
     init {
         this.capacity = capacity
         lastUsedKey = mutableListOf()
-        cacheHashMap = HashMap()
+        cacheHashMap = mutableListOf()
         print("null, ")
     }
 
     fun get(key: Int): Int {
-        val result = cacheHashMap[key]
-        print("${result?.first ?: -1}, ")
+        val result = cacheHashMap.findLast { key == it.first }
+        val resultIndex = cacheHashMap.indexOf(result)
+        print("${result?.second ?: -1}, ")
+//        println()
+//        cacheHashMap.forEach{
+//            println("(${it.first}, ${it.second})")
+//        }
         result?.let {
             result!!.second + 1
-            lastUsedKey += key
+            cacheHashMap[resultIndex] = Triple(result!!.first, result.second, result!!.third + 1)
+            lastUsedKey.add(key)
         }
 
-        return result?.first ?: -1
+        return result?.second ?: -1
     }
 
     fun put(key: Int, value: Int) {
-        if (currentCapacity < capacity)  {
-            cacheHashMap[key] = Pair(value, 1)
-            currentCapacity++
-            lastUsedKey += key
+//        println("\nstart")
+//        cacheHashMap.forEach {
+//            println("(${it.first}, ${it.third})")
+//        }
+
+        if (isKeyExist(key)) {
+            changeValueByKey(key, value)
+            lastUsedKey.add(key)
         } else {
-            val keyRemove = getLFU()
-            cacheHashMap.remove(keyRemove)
-            cacheHashMap[key] = Pair(value, 1)
-            lastUsedKey.removeAll{ it == keyRemove}
-            lastUsedKey += key
+            if (currentCapacity < capacity) {
+                cacheHashMap.add(Triple(key, value, 1))
+                currentCapacity++
+                lastUsedKey.add(key)
+            } else if (capacity != 0) {
+                val keyRemove = getLFU()
+//                print(" rem=$keyRemove ")
+                val element = getElement(keyRemove)
+                val elementIndex = getElementIndex(element!!)
+                cacheHashMap.remove(getElement(keyRemove))
+                cacheHashMap.add(Triple(key, value, 1))
+//                print(" new=($key, $value) ")
+                lastUsedKey.removeAll { it == keyRemove }
+                lastUsedKey.add(key)
+            }
         }
+
+//        println("end")
+//        cacheHashMap.forEach {
+//            println("(${it.first}, ${it.third})")
+//        }
         print("null, ")
     }
 
-    private fun getLFU(): Int{
+    private fun changeValueByKey(key: Int, value: Int) {
+        val element = getElement(key)
+        val elementIndex = getElementIndex(element!!)
+        cacheHashMap[elementIndex] = Triple(element.first, value, element.third + 1)
+    }
+
+    private fun isKeyExist(key: Int) = getElement(key) != null
+
+    private fun getElement(key: Int) = cacheHashMap.find { key == it.first }
+
+    private fun getElementIndex(triple: Triple<Int, Int, Int>) = cacheHashMap.indexOf(triple)
+
+    private fun getLFU(): Int {
         var list: MutableList<Pair<Int, Int>> = mutableListOf() // <key, counter>
         var index = 0
-        cacheHashMap.forEach { k, v ->
-            if (list.isEmpty()){
-                list += Pair(0, 0)
+        cacheHashMap.forEach { triple ->
+            if (list.isEmpty()) {
+                list.add(Pair(0, 10000))
             }
-            if (v.second > list[index].second){
-                if (index >= 1){
-                    list = mutableListOf(Pair(k, v.second))
+            if (triple.third < list[index].second) {
+                if (index >= 1) {
+                    list = mutableListOf(Pair(triple.first, triple.third))
                     index = 0
                 } else
-                    list[index] = Pair(k, v.second)
+                    list[index] = Pair(triple.first, triple.third)
             } else {
                 index++
-                list += Pair(k, v.second)
+                list.add(Pair(triple.first, triple.third))
             }
         }
 
-        if (list.size > 1){
+//        println("LFu")
+//        list.forEach{
+//            println(it)
+//        }
+        if (list.size > 1) {
             var index = -1
-            list.forEach {  pair ->
+            while (list.size > 1){
+                var indexInList = 0
+                index = -1
+                list.forEachIndexed() {indexaPair, pair ->
+                    val tempIndex = lastUsedKey.lastIndexOf(pair.first)
 
-                val tempIndex = lastUsedKey.find { it == pair.first }
+                    if (index == -1){
+                        index = tempIndex!!
+                        indexInList = indexaPair
+                    }
 
-                if (index == -1)
-                    index = tempIndex!!
 
-                if (tempIndex!! <= index){
-                    index = tempIndex
+                    if (tempIndex!! >= index) {
+                        index = tempIndex
+                        indexInList = indexaPair
+                    }
+//                    println("last = $tempIndex find = ${pair.first} index=${index}")
                 }
+                list.removeAt(indexInList)
             }
 
-            return lastUsedKey[index]
+//            list.forEach { pair ->
+//
+//                val tempIndex = lastUsedKey.indexOf(pair.first)
+//
+////                println("last = $tempIndex find = ${pair.first} size=${lastUsedKey.size}")
+////                lastUsedKey.forEachIndexed { index, value ->
+////                    println("($index, $value)")
+////                }
+//
+//                if (index == -1)
+//                    index = tempIndex!!
+//
+//                if (tempIndex!! <= index) {
+//                    index = tempIndex
+//                }
+//            }
+
+            return list[0].first
         } else {
             return list[0].first
         }
