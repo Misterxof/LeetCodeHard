7ae31cf16eca816812fa1c3b6dae5f89705665f1
Mikhail <mishail.lukushevich@gmail.com>
2023-02-09 21:50:50 +0300

    2306. Naming a Company
    Naming a Company
    - Runtime 1343 ms - 33.33%
    - Memory 60.9 MB - 33.33%

--- src/main/kotlin/t_th_z_s_Naming_Company/NamingCompany.kt
+++ src/main/kotlin/t_th_z_s_Naming_Company/NamingCompany.kt
@@ -3,21 +3,30 @@ package t_th_z_s_Naming_Company
 class NamingCompany {
     fun distinctNames(ideas: Array<String>): Long {
         var result = 0L
-        val hashMap = hashMapOf<String, Int>()
+        val hashMap = hashMapOf<Char, MutableSet<String>>()
+        val ignore = hashMapOf<Char, Int>() // skip that we already checked
 
         ideas.forEach {
-            hashMap[it] = 0
+            if (hashMap.containsKey(it[0]))
+                hashMap[it[0]]!!.add(it.replaceFirst(it[0].toString(), ""))
+            else
+                hashMap[it[0]] = mutableSetOf(it.replaceFirst(it[0].toString(), ""))
         }
 
-        ideas.forEachIndexed { i, it ->
-            ideas.forEachIndexed { j, jt ->
-                if (i != j) {
-                    val first = it.replaceFirst(it[0], jt[0])
-                    val second = jt.replaceFirst(jt[0], it[0])
-                    if (!hashMap.containsKey(first) && !hashMap.containsKey(second)) result++
+        hashMap.forEach { it ->
+            hashMap.forEach { jt ->
+                if (it.key != jt.key && !ignore.containsKey(jt.key)) {
+                    // find only unique members in two sets
+                    val r = it.value.minus(jt.value)
+                    val r2 = jt.value.minus(it.value)
+
+                    if (r.isNotEmpty() && r2.isNotEmpty())
+                        result += r.size * r2.size * 2
                 }
             }
+            ignore[it.key] = 0
         }
+
         return result
     }
 }
@@ -25,4 +34,5 @@ class NamingCompany {
 fun main() {
     println(NamingCompany().distinctNames(arrayOf("coffee","donuts","time","toffee")))
     println(NamingCompany().distinctNames(arrayOf("lack","back")))
+    println(NamingCompany().distinctNames(arrayOf("ps","yn","eciuehjexw","pdesv","e","dz","xxdnk","vdqjhncfj")))
 }
